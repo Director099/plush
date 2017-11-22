131a76df45bbf2d0464316568dc1e51aa786c787
Director099 <director099@gmail.com>
2017-11-22 11:37:13 +0500

    init

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(document).ready( function () {
+    $('#table_id').DataTable( {
+      "language": {
+          "url": '//cdn.datatables.net/plug-ins/1.10.16/i18n/Russian.json'
+        }
+    });
+} );
